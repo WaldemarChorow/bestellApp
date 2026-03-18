f115caf23a024debbbcc7199a0a59088463455e6
WaldemarChorow <chorow.w@gmail.com>
2026-03-18 16:09:00 +0100

    add: images:burger,pizza,salad change:db.js add: images url

--- scripts/db.js
+++ scripts/db.js
@@ -2,22 +2,26 @@ let menuBurger = [
     {
         "name": "Veggie mushroom black burger",
         "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
-        "price": 16.90
+        "price": 16.90,
+        "image": "assets/img/burgerIMGs/vaggie.svg"
     },
     {
         "name": "All meat burger",
         "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
-        "price": 15.90
+        "price": 15.90,
+        "image": "assets/img/burgerIMGs/allmeat.svg"
     },
     {
         "name": "Beef red burger",
         "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",
-        "price": 14.90
+        "price": 14.90,
+        "image": "assets/img/burgerIMGs/beef.svg"
     },
     {
         "name": "Big chicken burger",
         "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
-        "price": 15.90
+        "price": 15.90,
+        "image": "assets/img/burgerIMGs/chicken.svg"
     }
 ];
 
@@ -25,22 +29,26 @@ let menuSalad = [
     {
         "name": "Warm beef arugula salad",
         "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
-        "price": 16.90
+        "price": 16.90,
+        "image": "assets/img/saladIMGs/beef.svg"
     },
     {
         "name": "Mini green Salad",
         "description": "Green salad, Cucumber, Carrots, Parsley, Radishes",
-        "price": 7.90
+        "price": 7.90,
+        "image": "assets/img/saladIMGs/mini.svg"
     },
     {
         "name": "Green Salad with sea food",
         "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
-        "price": 16.90
+        "price": 16.90,
+        "image": "assets/img/saladIMGs/seefood.svg"
     },
     {
         "name": "Vegan green salad with tofu",
         "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
-        "price": 14.90
+        "price": 14.90,
+        "image": "assets/img/saladIMGs/tofu.svg"
     }
 ];
 
@@ -48,21 +56,25 @@ let menuPizza = [
     {
         "name": "Pizza Margherita",
         "description": "Tomato Sauce, Mozzarella",
-        "price": 11.90
+        "price": 11.90,
+        "image": "assets/img/pizzaIMGs/margherita.svg"
     },
     {
         "name": "Pizza Chorizo",
         "description": "Tomato slices, Mozzarella, Chorizo",
-        "price": 13.90
+        "price": 13.90,
+        "image": "assets/img/pizzaIMGs/chorizo.svg"
     },
     {
         "name": "Funghi",
         "description": "Red onion, Olives, Button Mushrooms, Mozzarella",
-        "price": 12.90
+        "price": 12.90,
+        "image": "assets/img/pizzaIMGs/funghi.svg"
     },
     {
         "name": "Quattro Formaggi with Chicken",
         "description": "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
-        "price": 15.90
+        "price": 15.90,
+        "image": "assets/img/pizzaIMGs/quattro.svg"
     }
 ];
