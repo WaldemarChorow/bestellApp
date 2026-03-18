d25decae29489faec305a5affda7d0b619b0e231
WaldemarChorow <chorow.w@gmail.com>
2026-03-18 15:33:12 +0100

    add: menu, head, description, price sections and schange the layout

---
+++ scripts/db.js
@@ -0,0 +1,68 @@
+let menuBurger = [
+    {
+        "name": "Veggie mushroom black burger",
+        "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+        "price": 16.90
+    },
+    {
+        "name": "All meat burger",
+        "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+        "price": 15.90
+    },
+    {
+        "name": "Beef red burger",
+        "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+        "price": 14.90
+    },
+    {
+        "name": "Big chicken burger",
+        "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+        "price": 15.90
+    }
+];
+
+let menuSalad = [
+    {
+        "name": "Warm beef arugula salad",
+        "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+        "price": 16.90
+    },
+    {
+        "name": "Mini green Salad",
+        "description": "Green salad, Cucumber, Carrots, Parsley, Radishes",
+        "price": 7.90
+    },
+    {
+        "name": "Green Salad with sea food",
+        "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+        "price": 16.90
+    },
+    {
+        "name": "Vegan green salad with tofu",
+        "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+        "price": 14.90
+    }
+];
+
+let menuPizza = [
+    {
+        "name": "Pizza Margherita",
+        "description": "Tomato Sauce, Mozzarella",
+        "price": 11.90
+    },
+    {
+        "name": "Pizza Chorizo",
+        "description": "Tomato slices, Mozzarella, Chorizo",
+        "price": 13.90
+    },
+    {
+        "name": "Funghi",
+        "description": "Red onion, Olives, Button Mushrooms, Mozzarella",
+        "price": 12.90
+    },
+    {
+        "name": "Quattro Formaggi with Chicken",
+        "description": "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
+        "price": 15.90
+    }
+];
